dbcdf16ee6db11729ad960929d526407b4fffa69
Facundo <facundo.vel99@gmail.com>
2025-06-21 02:48:29 -0300

    Se agrega algoritmo para ingresar numero e invertirlo

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Ingrese un numero: ");
+string numero = Console.ReadLine();
+bool esNumero = int.TryParse(numero, out int resultado);
+
+if(esNumero)
+{
+    if(resultado > 0)
+    {
+        int invertido = 0;
+        while (resultado > 0)
+        {
+            invertido = invertido * 10 + resultado % 10;
+            resultado /= 10;
+        }
+        Console.WriteLine("El numero invertido es: " + invertido);
+    }
+}
+
+
